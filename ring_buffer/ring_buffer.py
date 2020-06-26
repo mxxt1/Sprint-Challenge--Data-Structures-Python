16ee85bffa7c469ebfe1fd72adcc58cae00998b8
Matt Gill <matt@dustcloudlabs.com>
2020-06-26 19:16:20 -0400

    ring_buffer working correctly, passing test

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,11 +5,28 @@ class RingBuffer:
         # set fixed size for storage list. size = capacity
         self.storage = [None]*capacity
         #counter
-        self.current = None
+        self.counter = None
 
     def append(self, item):
-        if self.current is None:
-            pass
+        # start the buffer if it's empty
+        if self.counter is None:
+            # set position 0 to item
+            self.storage[0] = item
+            # increment counter
+            self.counter = 1
+        else:
+            # if buffer started set next slot to item and increment counter
+            self.storage[self.counter] = item
+            self.counter += 1
+            # if the buffer is at capacity, reset counter and start from beginning
+            if self.counter == self.capacity:
+                self.counter = 0
+        print(self.storage)
 
     def get(self):
-        pass
+        get_array = []
+        # for everything in storage, if it exists append it to the list
+        for x in self.storage:
+            if x is not None:
+                get_array.append(x)
+        return get_array
