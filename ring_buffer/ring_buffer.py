90c7953dd638840004d01b6c9e21386e89b78d15
Matt Gill <matt@dustcloudlabs.com>
2020-06-26 19:08:58 -0400

    ring_buffer init complete, list is correct size

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,15 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        # fixed size
+        self.capacity = capacity
+        # set fixed size for storage list. size = capacity
+        self.storage = [None]*capacity
+        #counter
+        self.current = None
 
     def append(self, item):
-        pass
+        if self.current is None:
+            pass
 
     def get(self):
         pass
