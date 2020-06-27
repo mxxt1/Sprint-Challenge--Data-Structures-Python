187d35bfb292a8e1c10dec53e3ef0e4153852cd3
Matt Gill <matt@dustcloudlabs.com>
2020-06-26 20:31:47 -0400

    STRETCH! .0043 seconds with set/intersection

--- names/names.py
+++ names/names.py
@@ -23,19 +23,29 @@ duplicates = []  # Return the list of duplicates in this data structure
 #O(n) * O(m) * O(1) * O(1)
 # Executes in .12168
 
+# MVP
+# # create tree
+# tree = BSTNode("A")
+# # loop and populate the tree with first list
+# for first in names_1: # O(n)
+#     tree.insert(first)
+# # loop through the second list and see if name exists in tree
+# for second in names_2: # O(m)
+# # if it the name exists, append it to the duplicates list
+#     if tree.contains(second): # O(1)
+#         duplicates.append(second) # O(1)
 
-# create tree
-tree = BSTNode("A")
-# loop and populate the tree with first list
-for first in names_1: # O(n)
-    tree.insert(first)
-# loop through the second list and see if name exists in tree
-for second in names_2: # O(m)
-# if it the name exists, append it to the duplicates list
-    if tree.contains(second): # O(1)
-        duplicates.append(second) # O(1)
+# # O(n+m)*O(1)*O(1) = O(n+m)
 
-# O(n*m)*O(1)*O(1) = O(n*m)
+# STRETCH
+# .0043 seconds!!!
+for name in set(map(str.rstrip,names_1)).intersection(map(str.rstrip,names_2)):
+    duplicates.append(name)
+
+# 1.397 seconds
+# for first in names_1:
+#     if first in names_2:
+#         duplicates.append(first)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -45,3 +55,8 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
+
+
+
